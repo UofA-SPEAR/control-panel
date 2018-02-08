2417b64aab5d37c440a77f71e170ab437c52bac9
Hudson Shykowski <huddy987@gmail.com>
2018-02-07 23:06:12 -0700

    "5 degrees of freedom"

--- ui/js/arm.js
+++ ui/js/arm.js
@@ -38,7 +38,7 @@ var arm_pieces = {
     seg2: {
         size: {
             x: 0.1,
-            y: 2.3,
+            y: 2.2,
             z: 0.1
         }
     },
@@ -102,9 +102,9 @@ function arm_setup() {
     arm_camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 
-    arm_update(Math.PI / 4, Math.PI / 6,  Math.PI / 6);
+    arm_update(Math.PI / 4, Math.PI / 6,  Math.PI / 6, Math.PI / 8, 0, Math.PI / 2.2);
 
-
+    //Add pieces to the scene
     arm_scene.add(arm_pieces.base.mesh);
     arm_scene.add(arm_pieces.seg1.mesh);
     arm_scene.add(arm_pieces.seg2.mesh);
@@ -113,48 +113,85 @@ function arm_setup() {
     arm_scene.add(arm_pieces.hand2.mesh);
 
     //Set up position of arm_pieces
-    arm_pieces.seg1.mesh.geometry.translate(0, arm_pieces.seg1.size.y / 2, 0);
-    arm_pieces.wrist.mesh.geometry.translate(0, arm_pieces.seg1.size.y, 0);
-    arm_pieces.hand1.mesh.geometry.translate(0, 5, 0);
-    arm_pieces.hand2.mesh.geometry.translate(0, 1, 0);
+    arm_pieces.seg1.mesh.geometry.translate(0, arm_pieces.seg1.size.y / 2, 0, Math.PI / 3);
 
-
-
-
+    //Set initial camera position
     arm_camera.position.z = 5;
 
     requestAnimationFrame(arm_render);
 
 }
 
-function arm_update(rotBase, rotSeg1 , offsetSeg2, /*offsetwrist, offsethand1, offsethand2 */ ) {
+function arm_update(rotBase, rotSeg1 , offsetSeg2, offsetWrist, spinWrist, offsetHand) {
+      //TODO: Get wrist to spin, get hand to open/close 
 
       //Set angles for each successive object based off of the previous one.
+      //TODO: Fix the rotational angles (Not representative right now)
       rotSeg2 = rotSeg1 - offsetSeg2
+      rotWrist = rotSeg2 - offsetWrist
+      rotHand = rotWrist - offsetHand
 
       //Reset rotations to 0 each frame
       arm_pieces.seg1.mesh.rotation.x = 0;
       arm_pieces.seg1.mesh.rotation.y = 0;
       arm_pieces.seg1.mesh.rotation.z = 0;
+
       arm_pieces.seg2.mesh.rotation.x = 0;
       arm_pieces.seg2.mesh.rotation.y = 0;
       arm_pieces.seg2.mesh.rotation.z = 0;
-      
-      //Translate geometries
-      arm_pieces.seg2.mesh.position.set(
-    + arm_pieces.seg1.size.y * Math.sin(rotBase) * Math.sin(rotSeg1),
-    + arm_pieces.seg2.size.y * Math.cos(rotSeg2) / 2 + (arm_pieces.seg1.size.y) * Math.cos(rotSeg1),
-    + arm_pieces.seg1.size.y * Math.cos(rotBase) * Math.sin(rotSeg1));
 
-     // (0,1,0) sets rotation (movement) around the base, (1,0,0) sets angle of vector
+      arm_pieces.wrist.mesh.rotation.x = 0;
+      arm_pieces.wrist.mesh.rotation.y = 0;
+      arm_pieces.wrist.mesh.rotation.z = 0;
+
+      arm_pieces.hand1.mesh.rotation.x = 0;
+      arm_pieces.hand1.mesh.rotation.y = 0;
+      arm_pieces.hand1.mesh.rotation.z = 0;
+
+      arm_pieces.hand2.mesh.rotation.x = 0;
+      arm_pieces.hand2.mesh.rotation.y = 0;
+      arm_pieces.hand2.mesh.rotation.z = 0;
+
+      //Translate seg2 to the tip of seg1
+      arm_pieces.seg2.mesh.position.set(
+      + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) / 2) * Math.sin(rotBase),
+      + arm_pieces.seg1.size.y * Math.cos(rotSeg1) + arm_pieces.seg2.size.y * Math.cos(rotSeg2) / 2,
+      + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) / 2) * Math.cos(rotBase));
+
+     //Translate wrist to the tip of seg2
+     arm_pieces.wrist.mesh.position.set(
+     + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) + arm_pieces.wrist.size.y * Math.sin(rotWrist) / 2) * Math.sin(rotBase),
+     + arm_pieces.seg1.size.y * Math.cos(rotSeg1) + arm_pieces.seg2.size.y * Math.cos(rotSeg2) + arm_pieces.wrist.size.y * Math.cos(rotWrist) / 2,
+     + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) + arm_pieces.wrist.size.y * Math.sin(rotWrist) / 2) * Math.cos(rotBase));
+
+     //Translate hand1 to the tip of seg2
+     arm_pieces.hand1.mesh.position.set(
+     + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) + arm_pieces.wrist.size.y * Math.sin(rotWrist) + arm_pieces.hand1.size.y * Math.sin(rotHand) / 2) * Math.sin(rotBase),
+     + arm_pieces.seg1.size.y * Math.cos(rotSeg1) + arm_pieces.seg2.size.y * Math.cos(rotSeg2) + arm_pieces.wrist.size.y * Math.cos(rotWrist) + arm_pieces.hand1.size.y * Math.cos(rotHand) / 2,
+     + (arm_pieces.seg1.size.y * Math.sin(rotSeg1) + arm_pieces.seg2.size.y * Math.sin(rotSeg2) + arm_pieces.wrist.size.y * Math.sin(rotWrist) + arm_pieces.hand1.size.y * Math.sin(rotHand) / 2) * Math.cos(rotBase));
+
+     //Translate hand2 to the tip of seg2
+     arm_pieces.hand2.mesh.position.set(arm_pieces.hand1.mesh.position.x, arm_pieces.hand1.mesh.position.y, arm_pieces.hand1.mesh.position.z);
+
+     //Set piece rotation around base
      arm_pieces.seg1.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotBase);
-     arm_pieces.seg1.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotSeg1);
      arm_pieces.seg2.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotBase);
+     arm_pieces.wrist.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotBase);
+     arm_pieces.hand1.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotBase);
+     arm_pieces.hand2.mesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), rotBase);
+
+     //Set angle between the base and the piece.
+     arm_pieces.seg1.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotSeg1);
      arm_pieces.seg2.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotSeg2);
+     arm_pieces.wrist.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotWrist);
+     arm_pieces.hand1.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotHand);
+     arm_pieces.hand2.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), rotHand);
+
+     //TODO: Spin the wrist on own axis (using quaternion?)
 }
 
 function arm_render() {
-    arm_update(Date.now() * 0.0005, Math.PI / 2, Math.PI / 4);
+    arm_update(Date.now() * 0.0005, Math.PI / 5, Math.PI / 6, Math.PI / 7, 0, Math.PI / 3);
 
     requestAnimationFrame(arm_render);
     arm_renderer.render(arm_scene, arm_camera);

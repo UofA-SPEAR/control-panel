72c965093bc81d49fdeb9582cc0bb47ad7fc1c9c
huddy987 <hudsonshykowski@Admins-MBP.hitronhub.home>
2018-02-11 13:01:18 -0700

    Add hand opening/closing - functionality is complete

--- ui/js/arm.js
+++ ui/js/arm.js
@@ -6,6 +6,7 @@ var arm_viewControls
 var arm_width;
 var arm_height;
 
+//Set default colors
 var arm_base_material = new THREE.MeshStandardMaterial({
     color: 0x222222,
     flatShading: true,
@@ -14,6 +15,13 @@ var arm_base_material = new THREE.MeshStandardMaterial({
 });
 var arm_material = new THREE.MeshStandardMaterial({
     color: 0xcccccc,
+    flatShading: true,
+    metalness: 0.3,
+    roughness: 0.8
+});
+
+var hand_material = new THREE.MeshStandardMaterial({
+    color: 0xff0000,
     flatShading: true,
     metalness: 0.3,
     roughness: 0.8
@@ -30,14 +38,14 @@ var arm_pieces = {
     seg1: {
         size: {
             x: 0.2,
-            y: 2,
+            y: 2.2,
             z: 0.2
         }
     },
     seg2: {
         size: {
             x: 0.1,
-            y: 2.2,
+            y: 2.0,
             z: 0.1
         }
     },
@@ -59,28 +67,28 @@ var arm_pieces = {
     hand1: {
       size: {
         x: 0.05,
-        y: 0.3,
+        y: 0.4,
         z: 0.05,
       }
     },
     hand2: {
       size: {
         x: 0.05,
-        y: 0.3,
+        y: 0.4,
         z: 0.05,
       }
     },
     hand3: {
       size: {
         x: 0.05,
-        y: 0.3,
+        y: 0.4,
         z: 0.05,
       }
     },
     hand4: {
       size: {
         x: 0.05,
-        y: 0.3,
+        y: 0.4,
         z: 0.05,
       }
     },
@@ -138,11 +146,9 @@ function arm_setup() {
     // make meshes
     for (let x in arm_pieces) {
         if (arm_pieces.hasOwnProperty(x)) {
-            arm_pieces[x].mesh = new THREE.Mesh(new THREE.BoxGeometry(arm_pieces[x].size.x, arm_pieces[x].size.y, arm_pieces[x].size.z), x == "base" ? arm_base_material : arm_material);
+            arm_pieces[x].mesh = new THREE.Mesh(new THREE.BoxGeometry(arm_pieces[x].size.x, arm_pieces[x].size.y, arm_pieces[x].size.z), x == "base" ? arm_base_material : x.includes("hand") ? hand_material : arm_material);
         }
     }
-
-
 
     // set up rotation controls
     let viewControls = new THREE.OrbitControls(arm_camera, arm_renderer.domElement);
@@ -152,7 +158,7 @@ function arm_setup() {
     arm_camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 
-    arm_update(Math.PI / 4, Math.PI / 6,  Math.PI / 6, Math.PI / 8, Math.PI / 2.2, 0);
+    arm_update(Math.PI / 4, Math.PI / 6,  Math.PI / 6, Math.PI / 8, Math.PI / 2.2, -Math.PI / 3, Math.PI / 5);
 
     //Add pieces to the scene
     arm_scene.add(arm_pieces.base.mesh);
@@ -172,16 +178,19 @@ function arm_setup() {
     //Set up the static position of the first piece
     arm_pieces.seg1.mesh.geometry.translate(0, arm_pieces.seg1.size.y / 2, 0);
 
+    //NOTE: If you wish to change the midpoint static angle simply edit the line below
+
+    statHand = Math.PI / 3
+
     //Set up static position of handMid children.
-    //NOTE: If the size  or static angle of these pieces are changed, these positions will need to be adjusted.
     arm_pieces.hand1.mesh.geometry.translate(0, 0.2, 0);
     arm_pieces.hand2.mesh.geometry.translate(0, 0.2, 0);
     arm_pieces.hand3.mesh.geometry.translate(0, 0.2, 0);
     arm_pieces.hand4.mesh.geometry.translate(0, 0.2, 0);
-    arm_pieces.hand5.mesh.geometry.translate(arm_pieces.hand1.size.y * Math.sin(Math.PI / 4), arm_pieces.hand1.size.y * Math.cos(Math.PI / 4) + arm_pieces.hand1.size.y / 2, 0);
-    arm_pieces.hand6.mesh.geometry.translate(-arm_pieces.hand2.size.y * Math.sin(Math.PI / 4), arm_pieces.hand1.size.y * Math.cos(Math.PI / 4) + arm_pieces.hand1.size.y / 2, 0);
-    arm_pieces.hand7.mesh.geometry.translate(0, arm_pieces.hand1.size.y * Math.cos(Math.PI / 4) + arm_pieces.hand1.size.y / 2, arm_pieces.hand3.size.y * Math.sin(Math.PI / 4));
-    arm_pieces.hand8.mesh.geometry.translate(0, arm_pieces.hand1.size.y * Math.cos(Math.PI / 4) + arm_pieces.hand1.size.y / 2, -arm_pieces.hand4.size.y * Math.sin(Math.PI / 4));
+    arm_pieces.hand5.mesh.geometry.translate(arm_pieces.hand1.size.y * Math.sin(statHand), arm_pieces.hand1.size.y * Math.cos(statHand) + arm_pieces.hand5.size.y / 2, 0);
+    arm_pieces.hand6.mesh.geometry.translate(-arm_pieces.hand2.size.y * Math.sin(statHand), arm_pieces.hand1.size.y * Math.cos(statHand) + arm_pieces.hand6.size.y / 2, 0);
+    arm_pieces.hand7.mesh.geometry.translate(0, arm_pieces.hand1.size.y * Math.cos(statHand) + arm_pieces.hand7.size.y / 2, arm_pieces.hand3.size.y * Math.sin(statHand));
+    arm_pieces.hand8.mesh.geometry.translate(0, arm_pieces.hand1.size.y * Math.cos(statHand) + arm_pieces.hand8.size.y / 2, -arm_pieces.hand4.size.y * Math.sin(statHand));
 
     //Set up children of midHand
     arm_pieces.handMid.mesh.add(arm_pieces.hand1.mesh)
@@ -193,20 +202,30 @@ function arm_setup() {
     arm_pieces.handMid.mesh.add(arm_pieces.hand7.mesh)
     arm_pieces.handMid.mesh.add(arm_pieces.hand8.mesh)
 
+    //Set up children of the first 4 mesh arm_pieces
+    arm_pieces.hand1.mesh.add(arm_pieces.hand7.mesh)
+    arm_pieces.hand2.mesh.add(arm_pieces.hand8.mesh)
+    arm_pieces.hand3.mesh.add(arm_pieces.hand6.mesh)
+    arm_pieces.hand4.mesh.add(arm_pieces.hand5.mesh)
+
     //Set initial camera position
     arm_camera.position.z = 5;
 
     requestAnimationFrame(arm_render);
 
 }
-
-function arm_update(rotBase, rotSeg1 , offsetSeg2, offsetWrist, offsetHand, spinHand){
-      //TODO: get hand to open/close, Add children to handMid
+//Please if you value your sanity DON'T touch any of the trig functions contained in this function
+function arm_update(rotBase, rotSeg1 , offsetSeg2, offsetWrist, offsetHand, spinHand, handOpen){
+      //TODO: Scale arm_pieces in accordance to real life values, recieve data from rover1
 
       //Set angles for each successive object based off of the previous one.
       rotSeg2 = rotSeg1 - offsetSeg2
       rotWrist = rotSeg2 - offsetWrist
       rotHand = rotWrist - offsetHand
+
+      //Set up static angles for the hand segments
+      //NOTE: If you wish to change the midpoint static angle simply edit the line below
+      statHand = Math.PI / 3
 
       //Reset rotations to 0 each frame
       arm_pieces.seg1.mesh.rotation.x = 0;
@@ -226,38 +245,39 @@ function arm_update(rotBase, rotSeg1 , offsetSeg2, offsetWrist, offsetHand, spin
       arm_pieces.handMid.mesh.rotation.z = 0;
 
       //NOTE: Angles are reset to Math.PI / 4 on purpose; so that the claw pieces have static offsets
-      arm_pieces.hand1.mesh.rotation.x = Math.PI / 4;
+      arm_pieces.hand1.mesh.rotation.x = statHand;
       arm_pieces.hand1.mesh.rotation.y = 0;
       arm_pieces.hand1.mesh.rotation.z = 0;
 
-      arm_pieces.hand2.mesh.rotation.x = -Math.PI / 4;
+      arm_pieces.hand2.mesh.rotation.x = -statHand;
       arm_pieces.hand2.mesh.rotation.y = 0;
       arm_pieces.hand2.mesh.rotation.z = 0;
 
       arm_pieces.hand3.mesh.rotation.x = 0;
       arm_pieces.hand3.mesh.rotation.y = 0;
-      arm_pieces.hand3.mesh.rotation.z = Math.PI / 4;
+      arm_pieces.hand3.mesh.rotation.z = statHand;
 
       arm_pieces.hand4.mesh.rotation.x = 0;
       arm_pieces.hand4.mesh.rotation.y = 0;
-      arm_pieces.hand4.mesh.rotation.z = -Math.PI / 4;
+      arm_pieces.hand4.mesh.rotation.z = -statHand;
 
-      //NOTE: If we decide to have static angle offsets of the top pieces, uncomment these lines and adjust the static positions
-      /*arm_pieces.hand5.mesh.rotation.x = 0;
+      //Since these pieces are children of the previous 4, the angle must be the negative of their parents so that they remain straight
+
+      arm_pieces.hand5.mesh.rotation.x = 0;
       arm_pieces.hand5.mesh.rotation.y = 0;
-      arm_pieces.hand5.mesh.rotation.z = 0;
+      arm_pieces.hand5.mesh.rotation.z = statHand;
 
-      arm_pieces.hand6.mesh.rotation.x = Math.PI / 6;
+      arm_pieces.hand6.mesh.rotation.x = 0;
       arm_pieces.hand6.mesh.rotation.y = 0;
-      arm_pieces.hand6.mesh.rotation.z = 0;
+      arm_pieces.hand6.mesh.rotation.z = -statHand;
 
-      arm_pieces.hand7.mesh.rotation.x = 0;
+      arm_pieces.hand7.mesh.rotation.x = -statHand;
       arm_pieces.hand7.mesh.rotation.y = 0;
-      arm_pieces.hand7.mesh.rotation.z = -Math.PI / 6;
+      arm_pieces.hand7.mesh.rotation.z = 0;
 
-      arm_pieces.hand8.mesh.rotation.x = 0;
+      arm_pieces.hand8.mesh.rotation.x = statHand;
       arm_pieces.hand8.mesh.rotation.y = 0;
-      arm_pieces.hand8.mesh.rotation.z = Math.PI / 6;*/
+      arm_pieces.hand8.mesh.rotation.z = 0;
 
       //Translate seg2 to the tip of seg1
       arm_pieces.seg2.mesh.position.set(
@@ -291,10 +311,18 @@ function arm_update(rotBase, rotSeg1 , offsetSeg2, offsetWrist, offsetHand, spin
 
      //Spin hand on it's own axis
      arm_pieces.handMid.mesh.rotateOnAxis(new THREE.Vector3(0, arm_pieces.handMid.mesh.position.y, 0).normalize(), spinHand);
+
+     //Open/close hand based on given angle. Negative angles: Close || Positive angles: Open
+
+     //TODO: Range of motion is variable depending on stathand, will need to be addressed when final scale is determined
+     arm_pieces.hand1.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), handOpen);
+     arm_pieces.hand2.mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), -handOpen);
+     arm_pieces.hand3.mesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), handOpen);
+     arm_pieces.hand4.mesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), -handOpen);
 }
 
 function arm_render() {
-    arm_update(Date.now() * 0.0005, Math.PI / 3, Math.PI / 6, Math.PI / 7, Math.PI / 4, Math.PI / 4);
+    arm_update(Date.now() * 0.0005, Math.PI / 3, Math.PI / 6, Math.PI / 7, Math.PI / 4, Math.PI / 4, -Math.PI / 8);
 
     requestAnimationFrame(arm_render);
     arm_renderer.render(arm_scene, arm_camera);

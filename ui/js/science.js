8ae3bfe106eff41b4c0322f9d9a53f62b2c0f7c0
Jacob Reckhard <jacobreckhard@gmail.com>
2018-02-28 16:31:27 -0700

    updated science view loading part

--- ui/js/science.js
+++ ui/js/science.js
@@ -1,11 +1,29 @@
+var moist; // data from moisture sensor
+var temp; // data from temperature sensor
+var uv; // data from uv sensor
+var gps; // data from gps data
+
 // Processes data recived from server before loading to html
 function science_processData(jsoData) {
     // any processing of data I.E. unit conversions, scaling, formating will go here
-    science_loadData(jsoData.moisture, jsoData.temperature, jsoData.uv, jsoData.gps);
+    if (jsoData.hasOwnProperty("moisture")) {
+        moist = jsoData.moisture;
+    }
+    if (jsoData.hasOwnProperty("gps")) {
+        gps = jsoData.gps;
+    }
+    if (jsoData.hasOwnProperty("uv")) {
+        uv = jsoData.uv;
+    }
+    if (jsoData.hasOwnProperty("temperature")) {
+        temp = jsoData.temperature;
+    }
+    science_refreshData();
 }
 
-// Loads the date recived from the server into the html
-function science_loadData(moist, temp, uv, gps) {
+
+// Loads the date received from the server into the html
+function science_refreshData() {
     document.getElementById("science-temperature").innerHTML = temp + "&#176;";
     document.getElementById("science-moisture").innerHTML = moist + "%";
     document.getElementById("science-uv").innerHTML = uv + " mW/m^2";

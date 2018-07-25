a9b79fad2156be9bc30dfc29f022139d2b227bc1
Jacob Reckhard <jacobreckhard@gmail.com>
2018-07-25 10:17:29 -0600

    updated old server code for testing

--- python/communicator.py
+++ python/communicator.py
@@ -1,6 +1,6 @@
 def handleSteeringDirections(steeringData):
     """Processes and sends steering data to the rover from the home base controller"""
-    speed = steeringData["netSpeed"]
-    angle = steeringData["netAngle"]
-    print("Sending speed(%f) and angle(%f)" % (speed, angle))
+    speed = steeringData["left"]
+    angle = steeringData["right"]
+    print("Sending left(%f) and right(%f)" % (speed, angle))
     # TODO: actually send data to rover
